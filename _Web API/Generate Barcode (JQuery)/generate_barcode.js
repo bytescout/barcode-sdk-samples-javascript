37727378907a559b5facbe190cb1cd9d50260f29
estar73 <estarkov@gmail.com>
2018-04-16 04:22:17 -0700

    = regular update

---
+++ _Web API/Generate Barcode (JQuery)/generate_barcode.js
@@ -0,0 +1,46 @@
+//*****************************************************************************************//
+//                                                                                         //
+// Download offline evaluation version (DLL): https://bytescout.com/download/web-installer //
+//                                                                                         //
+// Signup Web API free trial: https://secure.bytescout.com/users/sign_up                   //
+//                                                                                         //
+// Copyright © 2017-2018 ByteScout Inc. All rights reserved.                               //
+// http://www.bytescout.com                                                                //
+//                                                                                         //
+//*****************************************************************************************//
+
+
+$(document).ready(function () {
+    $("#resultBlock").hide();
+    $("#errorBlock").hide();
+});
+
+$(document).on("click", "#submit", function () {
+    var apiKey = $("#apiKey").val().trim(); //Get your API key by registering at http://www.bytescout.io/
+
+    var url = "https://bytescout.io/v1/barcode/generate?name=barcode.png";
+    url += "&type=" + $("#barcodeType").val(); // Set barcode type (symbology)
+    url += "&value=" + $("#inputValue").val(); // Set barcode value
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        headers: {
+            "x-api-key": apiKey
+        },
+    })
+    .done (function(data, textStatus, jqXHR) { 
+        if (data.error == false) {
+            $("#resultBlock").show();
+            $("#image").attr("src", data.url);
+        }
+        else {
+            $("#errorBlock").show();
+            $("#error").html(data.message);
+        }
+    })
+    .fail (function(jqXHR, textStatus, errorThrown) { 
+        $("#errorBlock").show();
+        $("#error").html("Request failed. Please check you use the correct API key.");
+    });
+});
---
+++ _Web API/Generate Barcode (Node.js)/GenerateBarcode.js
@@ -0,0 +1,65 @@
+//*****************************************************************************************//
+//                                                                                         //
+// Download offline evaluation version (DLL): https://bytescout.com/download/web-installer //
+//                                                                                         //
+// Signup Web API free trial: https://secure.bytescout.com/users/sign_up                   //
+//                                                                                         //
+// Copyright © 2017-2018 ByteScout Inc. All rights reserved.                               //
+// http://www.bytescout.com                                                                //
+//                                                                                         //
+//*****************************************************************************************//
+
+
+// (!) If you are getting "(403) Forbidden" error please ensure you have set the correct API_KEY
+
+var https = require("https");
+var path = require("path");
+var fs = require("fs");
+
+
+// The authentication key (API Key).
+// Get your own by registering at https://secure.bytescout.com/users/sign_up
+const API_KEY = "***********************************";
+
+
+// Result image file name
+const DestinationFile = "./barcode.png";
+// Barcode type. See valid barcode types in the documentation https://secure.bytescout.com/cloudapi.html#api-Default-barcodeGenerateGet
+const BarcodeType = "Code128";
+// Barcode value
+const BarcodeValue = "qweasd123456";
+
+
+// Prepare request to `Barcode Generator` API endpoint
+var queryPath = `/v1/barcode/generate?name=${path.basename(DestinationFile)}&type=${BarcodeType}&value=${BarcodeValue}`;
+var reqOptions = {
+    host: "bytescout.io",
+    path: encodeURI(queryPath),
+    headers: {
+        "x-api-key": API_KEY
+    }
+};
+// Send request
+https.get(reqOptions, (response) => {
+    response.on("data", (d) => {
+        // Parse JSON response
+        var data = JSON.parse(d)
+        
+        if (data.error == false) {
+            // Download the image file
+            var file = fs.createWriteStream(DestinationFile);
+            https.get(data.url, (response2) => {
+                response2.pipe(file).on("close", () => {
+                    console.log(`Generated barcode saved to '${DestinationFile}' file.`);
+                });
+            });
+        }
+        else {
+            // Service reported error
+            console.log(data.message);
+        }
+    });
+}).on("error", (e) => {
+    // Request error
+    console.error(e);
+});
